af9675eb9ec9cc287e8d216fd30f49db261b581d
Calladrus2001 <dugarvishesh@gmail.com>
2022-11-06 13:53:53 +0530

    Writing Test

--- routes/mongoose/Evaluation.js
+++ routes/mongoose/Evaluation.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 const History = require("../../models/History");
 const {evaluateYourself} = require("../../hedera/contract");
-const sentences = require('../../models/Evaluation');
+const {sentences, urls} = require('../../models/Evaluation');
 
 router.use(
   bodyParser.urlencoded({
@@ -12,35 +12,46 @@ router.use(
 );
 router.use(bodyParser.json());
 
-router.post("/reading", (req, res)=>{
-  const random = get3Random();
-  const userID = req.body.userID;
+router.get("/eval", (req, res)=>{
+  const userID = req.query.userID;
+  const type = req.query.type;
   try {
     evaluateYourself(userID);
-    addNewExpense("Reading", "Expense", 20, userID);
+    addNewExpense(type, "Expense", 20, userID);
   }
   catch(e){
     console.log(e);
     return res.sendStatus(500);
   }
-  return res.send({
+  if (type == "Reading"){
+    const random = get3Random(sentences);
+    return res.send({
     "sentences" : random
   });
+  }
+  else if (type == "Writing"){
+    return res.send({
+      "urls" : urls
+    });
+  }
+  else {
+    return res.sendStatus(404);
+  }
 });
 
-function get3Random(){
+function get3Random(array){
   const arr = [];
   var flag = 0;
   for (i = 0; i < 3; i++){
-    var idx = Math.floor(Math.random() * sentences.length);
+    var idx = Math.floor(Math.random() * array.length);
     for (j = 0; j < arr.length; j++){
-      if (sentences[idx] == arr[j]){
+      if (array[idx] == arr[j]){
         i -= 1;
         flag = 1;
         break;
       }
     }
-    if (flag == 0) arr.push(sentences[idx]);
+    if (flag == 0) arr.push(array[idx]);
     flag = 0;
   }
   return arr;
